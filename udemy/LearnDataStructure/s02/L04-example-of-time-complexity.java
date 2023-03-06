023a0bfe291a72dbe79db2f630c452d828b11eba
gsham <gsham@12cm.co.kr>
2023-03-06 20:32:55 +0900

    all

---
+++ udemy/LearnDataStructure/s02/L04-example-of-time-complexity.java
@@ -0,0 +1,35 @@
+
+
+
+
+// Explan Time Complexities
+
+// O(1)
+// O(log n)
+// O(n)
+// O(n logn)
+// O(n2)
+// O(n3)
+// O (2^n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ udemy/LearnDataStructure/s02/L05-Finding-Time-Complexity.java
@@ -0,0 +1,33 @@
+
+
+
+// How to Calculate 'Algorithm Time Complexity' ?
+
+// Iterative Algorithm
+// Recursive Algorithm
+
+//  FindBiggestNumber(int arr[]):
+//      biggestNumber = arr[0]
+//      loop i=1 to length(arr)-1
+//          if arr[i] > biggestNumber
+//              biggestNumber = arr[i]
+//      return biggestNumber
+
+// Example#1 : Time Complexity Of 'Iterative Algo(continued)'
+//  FindBiggestNumber(int arr[]):          
+//      biggestNumber = arr[0]       -- O(1)
+//      loop i=1 to length(arr)-1       -- O(n)
+//          if arr[i] > biggestNumber       -- O(1)
+//              biggestNumber = arr[i]      -- O(1)
+//      return biggestNumber    -- O(1)
+
+
+// Time Complexity = O(1) + O(n) + O(1)
+//                 = O(n)                  
+
+
+
+
+
+
+
